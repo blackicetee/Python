d50723e74eb68dcf3e6e1900e1901f294dddb03a
Thilo <blackicetee@gmx.de>
2016-12-14 11:09:40 +0100

    racecar is now written with classes and can also move up and down

--- python_games/race_game/race_game.py
+++ python_games/race_game/race_game.py
@@ -4,8 +4,9 @@ import pygame
 display_width = 800
 display_height = 600
 
-x_change = 0
+# __________Declaration of constants___________
 
+# Reference the path of a race car image
 CAR_IMAGE_NAME = 'transparent_car.png'
 
 # RGB defined variables RED GREEN BLUE(RGB) 
@@ -31,54 +32,87 @@ pygame.display.set_caption('A bit Racy')
 # The internal clock who measures the game time
 clock = pygame.time.Clock()
 
-car_image = pygame.image.load(CAR_IMAGE_NAME)
+class Car:
+    """ This class will handle the functionalities for a car like:
+        -drawing the car image on the main screen
+        -moving the car on key down and key up"""
 
-def car(x, y):
-	# blit will draw the image to the surface/main display at position (x, y)
-	gameDisplay.blit(car_image, (x, y))
+    # __________Declaration of member variables__________
 
-# Variables for coordinates on the screen
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+    # The coordinates for positioning a car image at the main screen
+    display_position_coordinate_x = 0
+    display_position_coordinate_y = 0
 
+    change_display_position_coordinate_x = 0
+    change_display_position_coordinate_y = 0
 
+    car_image = None
+    
+    def __init__(self, car_image, display_position_coordinate_x, display_position_coordinate_y):
+        """ Will initialize a car object with a car image for screen representation purpose and coordinates for the 
+        position"""
+        self.car_image = pygame.image.load(car_image)
+        self.display_position_coordinate_x = display_position_coordinate_x
+        self.display_position_coordinate_y = display_position_coordinate_y
 
-# Boolean variable to check if car is crashed or not
-crashed = False
+    def move_car_on_event_keypress_direction_arrows(self, event):
+        # Is there a key press at all?
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.change_display_position_coordinate_x = -5
+            elif event.key == pygame.K_RIGHT:
+                self.change_display_position_coordinate_x = 5
+            elif event.key == pygame.K_UP:
+                self.change_display_position_coordinate_y = -5
+            elif event.key == pygame.K_DOWN:
+                self.change_display_position_coordinate_y = 5
+        self.reset_car_movement_after_keypressing(event)
 
-# The main game loop: the business logic is written in this loop
-while not crashed:
-	# Returns every mouse or keyboard input event
-	for event in pygame.event.get():
-		# Checks if user want to quit the program
-		if event.type == pygame.QUIT:
-			crashed = True
-		
-		# Is there a key press at all?
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x_change = -5
-			elif event.key == pygame.K_RIGHT:
-				x_change = 5
-		
-		# Is there a key lifted up? (When you take the finger away from a key after pressing it)
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				x_change = 0
+    def reset_car_movement_after_keypressing(self, event):
+        # Is there a key lifted up? (When you take the finger away from a key after pressing it)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                self.change_display_position_coordinate_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                self.change_display_position_coordinate_y = 0
 
+    # will draw an image of the car to the main screen
+    def draw_car(self):
+        self.display_position_coordinate_x += self.change_display_position_coordinate_x
+        self.display_position_coordinate_y += self.change_display_position_coordinate_y
+        gameDisplay.blit(self.car_image, (self.display_position_coordinate_x, self.display_position_coordinate_y))        
 
-		x += x_change
+def game_loop():
+    # Boolean variable to check if car is crashed or not
+    game_exit = False
+    
+    # Initialize instance of car class
+    car = Car(CAR_IMAGE_NAME, (display_width * 0.45), (display_height * 0.8))
+    
+    #The main game loop: the business logic is written in this loop
+    while not game_exit:
+        # Returns every mouse or keyboard input event
+        for event in pygame.event.get():
+            # Checks if user want to quit the program
+            if event.type == pygame.QUIT:
+                game_exit = True
+                       
+            car.move_car_on_event_keypress_direction_arrows(event)
 
-		# Overwrites the complete screen with white even the car image so but it before showing the car
-		gameDisplay.fill(white)
+        # Overwrites the complete screen with white even the car image so but it before showing the car
+        gameDisplay.fill(white)
 
-		# Displays an image of a car at screen position (x, y) 
-		car(x, y)
-			
-	# Updates the main window
-	pygame.display.update()
-	# Parameter defines how much frames per second should be shown
-	clock.tick(60)
+        # Displays an image of a car at screen position (x, y) 
+        car.draw_car()
+                            
+        # Updates the main window
+        pygame.display.update()
+        
+        # Parameter defines how much frames per second should be shown
+        clock.tick(60)
+
+# Call of the main loop function (business logic is handled here)
+game_loop()
 
 # Special function for quitting pygame is like pygame.init() for correct quitting the game
 pygame.quit()
