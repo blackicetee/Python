7792bfe7ac0e6be06de8e6e6c7a3f72c5833163c
blackicetee <blackicetee@gmx.de>
2017-01-04 23:54:18 +0100

    update

--- python_games/simple_games/test_simple_games/test_reversi.py
+++ python_games/simple_games/test_simple_games/test_reversi.py
@@ -3,7 +3,7 @@ import unittest
 from python_games.simple_games.Reversi import Reversi
 
 
-class TestSuggestions(unittest.TestCase):
+class TestSuggestionsForBlackTokens(unittest.TestCase):
     def test_two_suggestions(self):
         reversi = Reversi()
         reversi.put_game_token('B', (3, 3))
@@ -40,9 +40,29 @@ class TestSuggestions(unittest.TestCase):
                          [(5, 1), (1, 3), (0, 2), (5, 4), (5, 0), (3, 7), (5, 3), (1, 1), (4, 2), (2, 4), (2, 6)])
 
 
+class TestSuggestionsForWhiteTokens(unittest.TestCase):
+    def test_multiple_suggestions(self):
+        reversi = Reversi()
+        reversi.put_game_token('B', (3, 3))
+        reversi.put_game_token('B', (4, 4))
+        reversi.put_game_token('B', (3, 2))
+        reversi.put_game_token('B', (3, 1))
+        reversi.put_game_token('W', (3, 4))
+        reversi.put_game_token('W', (4, 3))
+        reversi.put_game_token('W', (3, 5))
+        reversi.put_game_token('W', (3, 6))
+        reversi.put_game_token('W', (2, 2))
+        reversi.put_game_token('W', (1, 2))
+        reversi.put_game_token('W', (3, 0))
+        reversi.put_game_token('W', (4, 1))
+        self.assertEqual(reversi.suggest_all_moves('W'),
+                         [(4, 2), (5, 5), (4, 0), (5, 4), (5, 3), (2, 1), (2, 3), (4, 5), (2, 3), (2, 1)])
+
+
 def suite():
     """Returns an aggregation(called test suite)
     of all test cases in this test module"""
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestSuggestions)
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestSuggestionsForBlackTokens)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestSuggestionsForWhiteTokens)
 
-    return unittest.TestSuite([suite1])
+    return unittest.TestSuite([suite1, suite2])
